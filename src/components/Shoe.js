faebe4347a2ac2ad7edd1f6de79b5d1e0b5e3945
Deon Thomas <Deon.Thomas@absa.co.za>
2017-03-27 16:38:40 +0200

    Deon|Level1 - Fixed Test should render the price (to two decimal places)

--- src/components/Shoe.js
+++ src/components/Shoe.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 const Shoe = (props) => (
  
-   
         <div className="card-content">
           <span>{props.brand}</span>
           <span>{props.title}</span>
           <span>{props.name}</span>
-          <span>{props.price}</span>
+          <span>{props.price.toFixed(2)}</span>
         </div>
 );
 
