910aaa97b1961e232cb32f1e820292bffd3e8601
Deon Thomas <Deon.Thomas@absa.co.za>
2017-03-29 23:22:30 +0200

    Deon|Level3 - Fixed ✓ should render an <li> for each unique brand

--- src/components/Facet.js
+++ src/components/Facet.js
@@ -2,8 +2,18 @@ import React from 'react';
 import {countByKey} from '../utils';
 
 const Facet = (props) => (
-  <div>Facet</div>
+
+<div>
+<ul>
+{
+	countByKey(props.items,'brand').map((item)=>
+  		<li>{item.brand}</li>
+	)
+}
+	</ul>
+  </div>
 );
+
 
 Facet.propTypes = {
   items: React.PropTypes.array.isRequired,
